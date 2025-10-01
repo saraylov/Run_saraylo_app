02ee4b03e617c400f5ab33f2ea6d5cd00487ac8b
saraylov <saraylov@gmail.com>
2025-10-01 16:08:36 +0300

    ver. 0.0.7 (skelet web)

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  base: './', // Используем относительный путь для GitHub Pages
+  base: '', // Пустой базовый путь для GitHub Pages
 })
